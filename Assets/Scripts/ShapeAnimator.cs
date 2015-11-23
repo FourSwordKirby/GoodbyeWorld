7933590b64f7e8fa59fd4651f129259a184728af
Roger Liu <rogliu26@gmail.com>
2015-11-23 16:30:06 -0500

    started shape script stuff

---
+++ Assets/Scripts/ShapeAnimator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeAnimator : MonoBehaviour
+{
+    public float decayRate = 1.0f;
+    public float lifeSpan = 10.0f;
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Define animator.
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO if some conditions are met increase/decrease decay rate
+        lifeSpan -= Time.deltaTime * decayRate;
+        if (lifeSpan <= 0)
+        {
+            animator.SetBool("Dead", false);
+        }
+    }
+}
